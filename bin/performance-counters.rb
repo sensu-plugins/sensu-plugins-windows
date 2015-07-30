6ad5042553e46aba9f5f0beee7e9378830074b82
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2015-07-30 08:45:33 +0200

    Reverting to the more readable version

--- bin/performance-counters.rb
+++ bin/performance-counters.rb
@@ -65,7 +65,10 @@ class GenericMetric < Sensu::Plugin::Metric::CLI::Graphite
     IO.popen("typeperf -sc 1 #{flatten} ") do |io|
       CSV.parse(io.read, headers: true) do |row|
         row.shift
-        row.reject { |k, v| !v || !k }.each do |k, v|
+        row.each do |k, v|
+          next unless v && k
+          break if row.to_s.start_with? 'Exiting'
+
           key = k.split('\\', 4)[3]
           data = metrics.fetch("\\#{key}", nil)
           next unless data
