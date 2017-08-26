0817e7163b893d5f96f64075e7860b672cfe397b
hulkk <timo@hulkkonen.fi>
2017-08-26 21:25:20 +0300

    Pagefile check added for windows (#46)
    * Pagefile usage check added
    
    * check-windows-pagefile.ps1 added
    
    * Rename check-windows-pagefile.ps1.ps1 to check-windows-pagefile.ps1

---
+++ bin/powershell/check-windows-pagefile.ps1
@@ -0,0 +1,55 @@
+#
+#   check-windows-pagefile.ps1
+#
+# DESCRIPTION:
+#   This plugin collects the Pagefile Usage and compares against the WARNING and CRITICAL thresholds.
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Windows
+#
+# DEPENDENCIES:
+#   Powershell 3.0 or above
+#
+# USAGE:
+#   Powershell.exe -NonInteractive -NoProfile -ExecutionPolicy Bypass -NoLogo -File C:\\opt\\sensu\\plugins\\check-windows-pagefile.ps1 75 85
+#
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2016 sensu-plugins
+#   Released under the same terms as Sensu (the MIT license); see LICENSE for details.
+#
+
+#Requires -Version 3.0
+
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+   [int]$WARNING,
+
+   [Parameter(Mandatory=$True,Position=2)]
+   [int]$CRITICAL
+)
+
+$ThisProcess = Get-Process -Id $pid
+$ThisProcess.PriorityClass = "BelowNormal"
+
+[int]$pagefileAllocated = Get-WmiObject -Class Win32_PageFileUsage | Select -ExpandProperty AllocatedBaseSize
+[int]$pagefileCurrentUsage = Get-WmiObject -Class Win32_PageFileUsage | Select -ExpandProperty CurrentUsage
+
+[int]$Value = ($pagefileCurrentUsage/$pagefileAllocated) * 100
+
+If ($Value -gt $CRITICAL) {
+  Write-Host CheckWindowsPagefile CRITICAL: Pagefile usage at $Value%.
+  Exit 2 }
+
+If ($Value -gt $WARNING) {
+  Write-Host CheckWindowsPagefile WARNING: Pagefile usage at $Value%.
+  Exit 1 }
+
+Else {
+  Write-Host CheckWindowsPagefile OK: Pagefile usage at $Value%.
+  Exit 0 }
